31c3637b95f39b1ae068782eb3486d3b01e3e91f
BlackZu01 <citozuluaga44@gmail.com>
2024-09-19 17:50:43 -0500

    Question menu started

--- lib/welcome.dart
+++ lib/welcome.dart
@@ -11,7 +11,41 @@ class _QuestionPageState extends State<QuestionPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Center(child: Text('PAGINA PRINCIPAL'))),
+      appBar: AppBar(
+        title: const Center(
+            child: Text(
+          'Mi Mejor Ser',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        )),
+        centerTitle: true,
+        toolbarHeight: 110,
+        backgroundColor: Colors.lightGreenAccent,
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50))),
+        actions: [
+          PopupMenuButton(
+            itemBuilder: (context) => [
+              const PopupMenuItem(
+                child: Text('Editar'),
+              ),
+              const PopupMenuItem(
+                child: Text('Eliminar'),
+              )
+            ],
+          )
+        ],
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            )),
+      ),
     );
   }
 }
